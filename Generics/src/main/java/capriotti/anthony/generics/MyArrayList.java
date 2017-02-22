89cf3f64660117758ce9046c6ddb4e218069a01c
Anthony Capriotti <a.j.capriotti12@gmail.com>
2017-02-22 14:55:42 -0500

    Add files via upload
    MAL methods layed out test framework created

--- Generics/src/main/java/capriotti/anthony/generics/MyArrayList.java
+++ Generics/src/main/java/capriotti/anthony/generics/MyArrayList.java
@@ -3,5 +3,44 @@ package capriotti.anthony.generics;
 /**
  * Created by anthonycapriotti on 2/22/17.
  */
-public class MyArrayList {
+public class MyArrayList<T> {
+    private T[] myArray;
+    private int arraySize;
+    private int counter;
+
+    public MyArrayList(){
+        this(10);
+        arraySize = 0;
+    }
+
+    public MyArrayList(int index){
+        myArray = (T[])new Object[arraySize];
+        arraySize = arraySize;
+    }
+
+    public int getArraySize() {
+        return arraySize;
+    }
+
+    public boolean add(T typeObject){
+        return false;
+    }
+
+    public void add(int index, T typeObject ){}
+
+    public T get(int index){
+        return null;
+    }
+
+    public T remove(int index){ return null;}
+
+    public T set(int index, T typeObject){ return null;}
+
+    public void clear(){}
+
+    public boolean isEmpty(){ return false;}
+
+    public boolean contains(T typeObject){ return false;}
+
+
 }
--- Generics/src/test/java/capriotti/anthony/generics/MyArrayListTest.java
+++ Generics/src/test/java/capriotti/anthony/generics/MyArrayListTest.java
@@ -1,7 +1,83 @@
 package capriotti.anthony.generics;
 
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by anthonycapriotti on 2/22/17.
  */
 public class MyArrayListTest {
+    MyArrayList<String> mal = new MyArrayList<String>();
+
+
+    @Before
+    public void setUp(){
+        mal = new MyArrayList<String>();
+        mal.add(11, "eleven");
+    }
+
+    @Test
+    //add object
+    public void addObjectToArrayList(){
+        mal.add("stuff");
+        String expected = "stuff";
+        String actual = mal.get(0);
+        assertEquals("stuff will return", expected, actual);
+    }
+
+    @Test
+    //add object to specific element
+    public void addObjectToSpecificElementTest(){
+        mal.add(1,"fist index");
+        String expected = "first index";
+        String actual = mal.get(1);
+        assertEquals("first index will return", expected, actual);
+    }
+
+    @Test
+    public void getTest(){
+        int expectedArraySize = 11;
+        int actualArraySize = mal.getArraySize();
+        assertEquals(expectedArraySize,actualArraySize);
+    }
+
+    @Test
+    public void removeTest(){
+        mal.remove(11);
+        String expected = null;
+        String actual = mal.get(11);
+        assertEquals("null will return", expected, actual);
+    }
+
+    @Test
+    public void setTest(){
+        mal.set(9, "ninth index");
+        String expected = "ninth index";
+        String actual = mal.get(9);
+        assertEquals("ninth index returns", expected, actual);
+    }
+
+    @Test
+    public void clearTest(){
+        mal.clear();
+        String expected = null;
+        String actual = mal.get(0);
+        assertEquals("null will return", expected, actual);
+    }
+
+    @Test
+    public void isEmptyTest(){
+        assertFalse(mal.isEmpty());
+        mal.clear();
+        assertTrue(mal.isEmpty());
+    }
+
+    @Test
+    public void containsTest(){
+        assertTrue(mal.contains("eleven"));
+    }
+
+
+
 }
